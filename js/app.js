d634ce089d5a7aeef30283b019cd0c75bb78c5da
michaelannor <m.annor13@gmail.com>
2017-07-08 11:37:52 +0000

    completed portfolio site

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(document).foundation();
+
+  // function marginNote(element_id){
+  //
+  //   var margin_elements = document.querySelectorAll(".margin-text");
+  //   for (var i = 0; i < margin_elements.length; i++) {
+  //       margin_elements[i].style.display = "none";
+  //   }
+  //
+  //   var element = document.getElementById(element_id);
+  //   element.style.display = 'block';
+  //
+  //
+  // }
